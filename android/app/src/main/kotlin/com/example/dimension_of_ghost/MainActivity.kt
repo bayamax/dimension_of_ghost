5f21c2acacdb3dd0b249b86e4760ce22e1dfadcd
bayamax <96761258+bayamax@users.noreply.github.com>
2024-07-24 22:36:39 +0900

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/dimension_of_ghost/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dimension_of_ghost
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dimension of Ghost',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dimension of Ghost'),
+      ),
+      body: Center(
+        child: Text('Welcome to Dimension of Ghost!'),
+      ),
+    );
+  }
+}
